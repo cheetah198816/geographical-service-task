7b7eaa1ce5f570ef1b0f203b634721228fe22b54
Chetan Naik <cnaik011@gmail.com>
2017-12-29 22:27:15 +0100

    Final Commit

--- geographical-api-task/src/main/java/dto/response/RegisterJobResponse.java
+++ geographical-api-task/src/main/java/dto/response/RegisterJobResponse.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * Created by chetan on 22.12.2017.
  */
